a6ee496672fef02c8b823f9008bf558bbdc1f13f
Сергей <serega.iva@outlook.com>
2021-02-15 10:54:39 +0300

    Add Project

---
+++ TanksOnline/TanksOnline/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TanksOnline
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			PictureBox picture = new PictureBox();
+			this.Controls.Add(picture);
+		}
+	}
+}
