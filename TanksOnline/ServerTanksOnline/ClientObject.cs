288080aae2456e16cf122d33342bd68c6da19a6f
Сергей <serega.iva@outlook.com>
2021-02-20 19:03:50 +0300

    Отсылка действий на сервере

---
+++ TanksOnline/ServerTanksOnline/ClientObject.cs
@@ -0,0 +1,87 @@
+﻿using Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TanksOnline
+{
+	class ClientObject
+	{
+		public string Id { get; set; }
+		public NetworkStream Stream { get; set; }
+		public ObjectAction objectAction { get; set; }
+		TcpClient client;
+		private ServerObject serverObject;
+
+		private delegate void SomeAction();
+		public ClientObject(TcpClient client,ServerObject serverObject)
+		{
+			objectAction = new ObjectAction();
+			this.client = client;
+			this.serverObject = serverObject;
+			this.Id = Guid.NewGuid().ToString();
+			serverObject.AddConnection(this);
+
+		}
+		internal void Process()
+		{
+			try
+			{
+				Stream = client.GetStream();
+				Object temp;
+
+
+				while (true)
+				{
+					byte[] data = new byte[1024];
+					do
+					{
+						Stream.Read(data, 0, data.Length);
+						temp = Serialization.Serialization.ByteArrayToObject(data);
+
+					} while (Stream.DataAvailable);
+					objectAction = (ObjectAction)temp;
+					NewAction();
+					SendAction();
+					serverObject.ActionBroadCast(Serialization.Serialization.ObjectToByteArray(serverObject),this.Id);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message+ " Process");
+			}
+		}
+		internal void SendAction()
+		{
+			byte[] data = Serialization.Serialization.ObjectToByteArray(objectAction);
+			Stream.Write(data, 0, data.Length);
+		}
+		internal ObjectAction NewAction()
+		{
+			if (objectAction.Up)
+			{
+				objectAction.Y += objectAction.Speed;
+
+			}
+			if (objectAction.Down)
+			{
+				objectAction.Y -= objectAction.Speed;
+			}
+			if (objectAction.Left)
+			{
+				objectAction.X -= objectAction.Speed;
+			}
+			if (objectAction.Righ)
+			{
+				objectAction.X += objectAction.Speed;
+			}
+
+			return objectAction;
+		}
+	}
+}
---
+++ TanksOnline/ServerTanksOnline/ServerObject.cs
@@ -0,0 +1,63 @@
+﻿using Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TanksOnline
+{
+	class ServerObject
+	{
+		static TcpListener tcpListener;
+		List<ClientObject> clients = new List<ClientObject>();
+		protected internal void AddConnection(ClientObject clientObject)
+		{
+			clients.Add(clientObject);
+		}
+		protected internal void RemoveConnection(string Id)
+		{
+			ClientObject client = clients.FirstOrDefault(x => x.Id == Id);
+			if (client != null) clients.Remove(client);
+		}
+		//Отправить действие
+		protected internal void ActionBroadCast(byte[]data,string id)
+		{
+			for (int i = 0; i < clients.Count; i++)
+			{
+				if (clients[i].Id != id)
+					clients[i].Stream.Write(data, 0, data.Length);
+			}
+		}
+		protected internal void Listen()
+		{
+			try
+			{
+				string IP = Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(x => x.AddressFamily == AddressFamily.InterNetwork).ToString();
+
+				tcpListener = new TcpListener(IPAddress.Any, 8000);
+				tcpListener.Start();
+				while(true)
+				{
+					TcpClient tcpClient = tcpListener.AcceptTcpClient();
+					ClientObject clientObject = new ClientObject(tcpClient, this);
+					Thread thread = new Thread(new ThreadStart(clientObject.Process));
+					thread.Start();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + " Listen");
+				Disconnect();
+			}
+		}
+		protected internal void Disconnect()
+		{
+		
+		}
+	}
+}
